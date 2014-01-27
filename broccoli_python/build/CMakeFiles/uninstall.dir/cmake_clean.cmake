6f6a2fdb3f720456ee8d5cf36c661df6ea92a304
antsankov <antsankov@gmail.com>
2014-01-27 16:17:41 -0700

    First

---
+++ broccoli_python/build/CMakeFiles/uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "broccoli_internPYTHON_wrap.c"
+  "broccoli_intern.py"
+  "CMakeFiles/uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
