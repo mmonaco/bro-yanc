abcff99983e70b9baaef02766ea5a1bb0a6d48f5
antsankov <antsankov@gmail.com>
2014-02-08 19:27:34 -0700

    Getting reciever to work

--- tests/record_reciever.py
+++ tests/record_reciever.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python
 
 import time as Time
-from broccoli import *
 
-bc = Connection("127.0.0.1:47758") 
+from broccoli import *
 
 @event
 def test2(a,b,c,d,e,f,g,h,i,j,i6,j6):
@@ -23,7 +22,75 @@ def test2(a,b,c,d,e,f,g,h,i,j,i6,j6):
     print repr(i6), i6
     print repr(j6), j6
 
-test2
+@event(int,count,time,interval,bool,double,addr,port,addr,subnet,addr,subnet)
+def test2b(a,b,c,d,e,f,g,h,i,j,i6,j6):
+    print "==== atomic b %d ====" % recv
+    print repr(a), a
+    print repr(b), b
+    print repr(c), "%.4f" % c.val
+    print repr(d), d
+    print repr(e), e
+    print f
+    print repr(g), g
+    print repr(h), h
+    print repr(i), i
+    print repr(j), j
+    print repr(i6), i6
+    print repr(j6), j6
 
-bc.processInput();
- 
+rec = record_type("a", "b")
+other_rec = record_type("a")
+
+# @event(rec)
+# def test4(r):
+#     global recv
+#     recv += 1
+#     print "==== record %d ====" % recv
+#     print repr(r)
+#     print repr(r.a), r.a
+#     print repr(r.b), r.b
+
+bc = Connection("127.0.0.1:47758")
+
+bc.send("test1",
+    int(-10),
+    count(2),
+    time(current_time()),
+    interval(120),
+    bool(False),
+    double(1.5),
+    string("Servus"),
+    port("5555/tcp"),
+    addr("6.7.6.5"),
+    subnet("192.168.0.0/16"),
+    addr("2001:db8:85a3::8a2e:370:7334"),
+    subnet("2001:db8:85a3::/48")
+    )
+
+recv = 0
+while True:
+    bc.processInput();
+    if recv == 1: #this is the number of recieving data grams it is looking for. 2 for 2 tests
+        break
+    Time.sleep(1)
+
+
+# r = record(rec)
+# r.a = 42;
+# r.b = addr("6.6.7.7")
+
+# bc.send("test3", r)
+
+# recv = 0
+# while True:
+#     bc.processInput();
+#     if recv == 2:
+#         break
+#     Time.sleep(1)
+
+# opt_record = record_type("one", "a", "b", "c", "d")
+# r = record(opt_record)
+# r.a = 13
+# r.c = "helloworld"
+
+# bc.send("test5", r)
