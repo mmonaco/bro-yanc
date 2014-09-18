bb99adbb2cd8d713b50c686f41277fcd6c22bc50
antsankov <antsankov@gmail.com>
2014-09-17 18:11:41 -0600

    added more support for ls_var

--- terminal_app.py
+++ terminal_app.py
@@ -78,8 +78,12 @@ def term():
 	print ("\nTerminal to Bro Device. Type 'help' for commands")
 
 	while True:	
-		n = raw_input("\n>> ")
 
+		if current_connection:
+			n = raw_input("\n" + current_connection.name + " >> ")
+
+		else:
+			n = raw_input("\n" +  ">> ")
 		split_line = (n.lower()).split()
 
 		#add new commands here so we can tell valid/invalid commands 
@@ -88,148 +92,141 @@ def term():
 					"send_script","module","modify_script","demo","set_string","set_int"]
 
 		#Comments on top of splitline is syntax
-		#try:
-		# quit
-		if split_line[0] == "quit":
-			break
+		try:
+			# quit	
+			if split_line[0] == "quit":
+				break
 
-		# help
-		if split_line[0] == "help":
-			do_help(split_line)
+			# help
+			if split_line[0] == "help":
+				do_help(split_line)
 
-		#set_connection (connection_name)	
-		if split_line[0] == "set_connection":
-			try:
+			#set_connection (connection_name)	
+			if split_line[0] == "set_connection":
+				try:
+					for connection in open_connections:
+						print ("connection.name is: " + connection.name + "arg is " + split_line[1])
+						if connection.name == split_line[1]:
+							current_connection=connection
+							break
+						else:
+							print("connection name not found")
+				except:
+					print("Include a name with the following format: set_connection local")
+
+			#current_connection 		
+			if split_line[0] == "current_connection":
+				if (current_connection):
+					print ("Connected to: " + current_connection.ip_port + " with " + current_connection.name)
+				else:
+					print("No current connection to any device, maybe use the 'connect' command?")
+			
+			#connections
+			if split_line[0] == "connections":
+				for i in open_connections:
+					print i.name
+
+			#connect 127.0.0.1:47758 bro1
+			if split_line[0] == "connect":
+				make = True
+
+				#these are shortcut connections for testing purposes. The scripts are generally written to listen on 127.0.0.1:47758, so typing "connect bro1" will connect you without having type much 
+				if split_line[1] == "bro1":
+					open_connections.append(bro_connection("127.0.0.1:47758","bro1"))
+					open_connections[-1].open_connection()
+					current_connection = open_connections[-1]
+
+				#This is used for making sure no duplicate connection of the same name or ip is made 
 				for connection in open_connections:
-					print ("connection.name is: " + connection.name + "arg is " + split_line[1])
-					if connection.name == split_line[1]:
-						current_connection=connection
-						break
-					else:
-						print("connection name not found")
-			except:
-				print("Include a name with the following format: set_connection local")
+					if (connection.name == split_line[2] or connection.ip_port == split_line[1]):
+						make = False
+						
+						if not make:
+							print("Connection already made at: " + connection.ip_port + " with name " + connection.name)
 
-		#current_connection 		
-		if split_line[0] == "current_connection":
-			if (current_connection):
-				print ("Connected to: " + current_connection.ip_port + " with " + current_connection.name)
+				#if there isn't a name or ip conflict 
+				if (make):
+					open_connections.append(bro_connection(split_line[1],split_line[2]))
+					open_connections[-1].open_connection()
+					current_connection=open_connections[-1]
+
+			
+			if not split_line[0] in commands:
+				print ("Do you need help? Type 'help' for a list of possible commands.")
+
+			#These commands require there to be an active connection to work 
+			if (current_connection): 	
+				# ls_var, right now this only works with address table. I will work to find a way to get other tables later. 
+				if split_line[0] == "ls_var": 
+					temp_dict.clear()
+					do_update_waiter(current_connection)
+					do_list(current_connection)
+
+				# list
+				if split_line[0] == "list": 
+					do_list(current_connection)
+
+				# set_addr foo 255.255.255.255
+				if split_line[0] == "set_var": 
+					do_set_addr(split_line,current_connection)
+
+				# set_int foo 4
+				if split_line[0] == "set_int": 
+					do_set_int(split_line,current_connection)
+
+				# set_string foo bar 
+				if split_line[0] == "set_string": 
+					do_set_string(split_line,current_connection)
+
+				# delvar foo 255.255.255.255
+				if split_line[0] == "delvar": 
+					do_delvar(split_line,current_connection)
+
+				# update_all
+				if split_line[0] == "update_all":
+					do_update_all()
+
+				if split_line[0] == "demo":
+					do_demo(current_connection)
+
+			########################################
+			#These are for prepping/sending scripts#
+			########################################
+
+			# module (name) (port)
+			if split_line[0] == "module":
+				do_create_module(split_line[1],split_line[2])
+
+			# modify_script (bro file) (module_name)
+			if split_line[0] == "modify_script":
+				do_modify_script(split_line[1],split_line[2])
+
+			# send_script (Dest ip) (Dest username) (Dest password) (brofile localpath) (dest filename)
+			if split_line[0] == "send_script":
+				print("howdy")
+				do_send_script("127.0.0.1","user","user","/home/user/Documents/broc_yanc/broccoli_python/testfile","twit")
+
+		#this is if they are trying to press enter with nothing actually in the line
+		except IndexError:
+
+			print("Make sure your command has the proper arguments!")
+
+		#this is for when user is entering an invalid ip:port string
+		except ValueError:
+			print("Your ip/port is invalid, use the proper syntax: 127.0.0.1:47758")
+
+		#this handles an error thrown when we try to make a connection from a the python script to an invalid bro device
+		except socket.error:
+			print ("Your I{} address in invalid")
+
+		# #When trying to connect to a bro device fails
+		except IOError:
+			#if user has ever even initated a connection
+			if(current_connection):
+				print ("Can't reach the bro device at: " + current_connection.name + " with " + current_connection.ip_port)
+		
 			else:
-				print("No current connection to any device, maybe use the 'connect' command?")
-		#connections
-		if split_line[0] == "connections":
-			for i in open_connections:
-				print i.name
-
-		#connect 127.0.0.1:47758 bro1
-		if split_line[0] == "connect":
-			make = True
-
-			if split_line[1] == "bro1":
-				open_connections.append(bro_connection("127.0.0.1:47758","bro1"))
-				open_connections[-1].open_connection()
-				current_connection = open_connections[-1]
-
-			if split_line[1] == "bro2":
-				open_connections.append(bro_connection("127.0.0.1:47759","bro2"))
-				open_connections[-1].open_connection()
-				current_connection = open_connections[-1]
-
-			#This is used for making sure no duplicate connection is made 
-			for connection in open_connections:
-				if (connection.name == split_line[2] or connection.ip_port == split_line[1]):
-					make = False
-					
-					if not make:
-						print("Connection already made at: " + connection.ip_port + " with name " + connection.name)
-
-			if (make):
-				open_connections.append(bro_connection(split_line[1],split_line[2]))
-				open_connections[-1].open_connection()
-				current_connection=open_connections[-1]
-
-		
-		if not split_line[0] in commands:
-			print ("Do you need help? Type 'help' for a list of possible commands.")
-
-		#These commands require there to be an active connection to work 
-
-		if (current_connection): 	
-			# ls_var, right now this only works with address table. I will work to find a way to get other tables later. 
-			if split_line[0] == "ls_var": 
-				temp_dict.clear()
-				do_update_waiter(current_connection)
-
-			# list
-			if split_line[0] == "list": 
-				do_list(current_connection)
-
-			# set_addr foo 255.255.255.255
-			if split_line[0] == "set_var": 
-				do_set_addr(split_line,current_connection)
-
-			# set_int foo 4
-			if split_line[0] == "set_int": 
-				do_set_int(split_line,current_connection)
-
-			# set_string foo bar 
-			if split_line[0] == "set_string": 
-				do_set_string(split_line,current_connection)
-
-			# delvar foo 255.255.255.255
-			if split_line[0] == "delvar": 
-				do_delvar(split_line,current_connection)
-
-			# update_all
-			if split_line[0] == "update_all":
-				do_update_all()
-
-			if split_line[0] == "demo":
-				do_demo(current_connection)
-
-		########################################
-		#These are for prepping/sending scripts#
-		########################################
-
-		# module (name) (port)
-		if split_line[0] == "module":
-			do_create_module(split_line[1],split_line[2])
-
-		# modify_script (bro file) (module_name)
-		if split_line[0] == "modify_script":
-			do_modify_script(split_line[1],split_line[2])
-
-		# send_script (Dest ip) (Dest username) (Dest password) (brofile localpath) (dest filename)
-		if split_line[0] == "send_script":
-			print("howdy")
-			do_send_script("127.0.0.1","user","user","/home/user/Documents/broc_yanc/broccoli_python/testfile","twit")
-
-		# #this is if they are trying to press enter with nothing actually in the line
-		# except IndexError:
-
-		# 	#this is when i make the quick connection via "connect bro1" or "connect bro2"
-		# 	if (split_line[1] == "bro1" or "bro2"):
-		# 		print("shortcut connection made")
-
-		# 	else:
-		# 		print("Make sure your command has the proper arguments!")
-
-		# #this is for when user is entering an invalid ip:port string
-		# except ValueError:
-		# 	print("Your ip/port is invalid, use the proper syntax: 127.0.0.1:47758")
-
-		# #this handles an error thrown when we try to make a connection from a the python script to an invalid bro device
-		# except socket.error:
-		# 	print ("Your I{} address in invalid")
-
-		# # #When trying to connect to a bro device fails
-		# except IOError:
-		# 	#if user has ever even initated a connection
-		# 	if(current_connection):
-		# 		print ("Can't reach the bro device at: " + current_connection.name + " with " + current_connection.ip_port)
-		
-		# 	else:
-		# 		print ("Can't contact bro device")
+				print ("Can't contact bro device")
 
 ###########################################################################################
 #END OF TERMINAL APP#######################################################################
@@ -238,7 +235,6 @@ def term():
 #This is the method that recieves the update information from bro and populates the dictionary 
 @event
 def update_strings(variable_name,value):
-	print("recieved " + variable_name + " and " + value)
 
 	if not (variable_name == "default_string" and value == "null"):
 		temp_dict[variable_name] = value
@@ -247,10 +243,8 @@ def update_strings(variable_name,value):
 	global recieved
 	recieved += 1 
 
-
 @event
 def update_addrs(variable_name,value):
-	print("recieved " + variable_name + " and " + value)
 
 	if not (variable_name == "default_addr" and value == "0.0.0.0"):
 		temp_dict[variable_name] = value
@@ -259,22 +253,18 @@ def update_addrs(variable_name,value):
 	global recieved
 	recieved += 1 
 
-
 @event
 def update_ints(variable_name,value):
-	print("recieved " + variable_name + " and " + value)
 
 	if not (variable_name == "default_int" and value == 0):
-		temp_dict[variable_name] = value
+		#we have to cast it to an int, because brocoli transforms the int coming from the bro device to a long. We cast it back again.
+		temp_dict[variable_name] = int(value)
 
 	#when we recieved the info from bro, we incrmenet the recieved counter by 1. 
 	global recieved
 	recieved += 1 
 
-
-
-
-#This waits for the update info to be sent from the bro device. 
+#This waits for the update info to be sent from the bro device.
 def do_update_waiter(bro_connection):
 
 	global temp_dict
@@ -285,11 +275,8 @@ def do_update_waiter(bro_connection):
 
 	#response are saved globally in temp_dict (this is becaue I couldn't get bro to send me the individual connection objects for each connection)
 	bro_connection.var_dict = temp_dict
-	#the temp dict is then put into the connection object, and the temp dict is cleared for the next connection to add to it. 
 	temp_dict.clear
 
-	do_list(bro_connection)
-	#list out the stuff in the dictionary
 
 #This sends the variable to the bro device to be added to the user set. 
 def do_set_addr(split_line,bro_connection):
@@ -347,7 +334,8 @@ def do_list(bro_connection):
 def do_update_all():
 	for connection in open_connections:
 		do_update_waiter(connection)
-		print("++++++++++++++++++")
+		print ("Device " + connection.name + " list of vars are- " )
+		do_list(connection)
 
 #This takes the template module from /yanc/main.bro and edits features to create a custom module for a bro script 
 def do_create_module(name,port):
